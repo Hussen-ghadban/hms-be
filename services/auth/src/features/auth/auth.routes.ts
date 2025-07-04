de7c998f1a746bb6ea9df1a5f32bf37d36601c42
majidonaisy <onaisym4@gmail.com>
2025-07-04 14:04:29 +0300

    fix

--- services/auth/src/features/auth/auth.routes.ts
+++ services/auth/src/features/auth/auth.routes.ts
@@ -1,6 +1,6 @@
 // src/routes/auth.ts
 import { Router } from 'express';
-import { authenticate, login, addUser, getUser } from './auth.controller';
+import { authenticate, login, addUser, getUser, employees } from './auth.controller';
 import { actionLogger } from '../../middleware/logger';
 import { requirePermissions } from '../../middleware/authenticate';
 import { validateRequest } from '../../middleware/validation';
@@ -12,5 +12,5 @@ router.post('/login', login,actionLogger("login"));
 router.post('/add-user',requirePermissions(["User.create"]), validateRequest({ body: createUserSchema }),  addUser,actionLogger("add user"));
 router.get('/get-user/:id',requirePermissions(["User.read"]), getUser, actionLogger("get user"))
 router.post('/services/authenticate',authenticate);
-router.get('/employees', requirePermissions(["User.read"]),getUser, actionLogger("Query Employees")); 
+router.get('/employees', requirePermissions(["User.read"]),employees, actionLogger("Query Employees")); 
 export default router;
